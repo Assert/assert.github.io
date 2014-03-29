d573492b66e06b3925d37ba2e80955c36acd497f
Eystein Bye <eysteinbye@gmail.com>
2014-03-29 20:58:12 +0100

    ./publish.sh

---
+++ publish.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+
+
+
+if [ "$0" == "" ]; then
+
+	echo "You need to add a message for the commit to GitHub"
+	echo ">publish 'New blog post about MarkDown'"
+
+else
+
+
+	echo "Building site with Jekyll"
+
+	jekyll serve
+
+	echo "Pushing to Git"
+
+	git add --all
+	git commit -m $0
+	git push origin master
+fi
+
+
